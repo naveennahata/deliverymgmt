ce25a10e0b0c7e0c82afb933d05fff0f33261dbd
NAVEEN NAHATA <naveen.nahata@MacBook-Pro-2.local>
2017-02-26 02:30:24 +0530

    Adding lpe client

--- careem/promise-engine/src/main/java/com/careem/hackathon/promise/engine/modal/Plan.java
+++ careem/promise-engine/src/main/java/com/careem/hackathon/promise/engine/modal/Plan.java
@@ -1,11 +1,14 @@
 package com.careem.hackathon.promise.engine.modal;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import java.util.Date;
 
 /**
  * Created by naveen.nahata on 26/02/17.
  */
 public class Plan {
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy", locale = "en_IN", timezone = "Asia/Calcutta")
     Date estimated_time_delivery;
     double cost;
 }
