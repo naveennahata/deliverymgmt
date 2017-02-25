038071be7d80a02e2c7f5f608cb448f10155784e
Rishabh Sood <rishabh.sood@MacBook-Pro-3.local>
2017-02-25 20:01:40 +0530

    adding modules

---
+++ careem/consignmentservice/src/main/java/com/careem/hackathon/api/ConsignmentRequest.java
@@ -0,0 +1,18 @@
+package com.careem.hackathon.api;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by rishabh.sood on 25/02/17.
+ */
+@Data
+@NoArgsConstructor
+public class ConsignmentRequest {
+    private Integer userId;
+    private Integer startLocation;
+    private Integer endLocation;
+    private List<String> shipmentIds;
+}
